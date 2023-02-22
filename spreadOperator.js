8fb34403f90a5bb17c00ada522d0abe9bd571238
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-22 15:33:48 +0700

    fundamental JS

---
+++ spreadOperator.js
@@ -0,0 +1,13 @@
+// Spread Operator
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+
+console.log(favorites);
+
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup' ]
+*/
+
+console.log(...favorites);
+/* output
+Seafood Salad Nugget Soup
+*/
