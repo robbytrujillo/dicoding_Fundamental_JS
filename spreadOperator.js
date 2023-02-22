658c660bc5690d41d242644aed5e3e4b6110657d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-22 15:54:33 +0700

    fundamental JS

--- spreadOperator.js
+++ spreadOperator.js
@@ -1,5 +1,9 @@
 // Spread Operator
 const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+const others = ["Cake", "Pie", "Donut"];
+
+const allFaforites = [favorites, others];
+const allFaforites1 = [...favorites, ...others];
 
 console.log(favorites);
 
@@ -10,4 +14,27 @@ console.log(favorites);
 console.log(...favorites);
 /* output
 Seafood Salad Nugget Soup
+*/
+
+console.log(allFaforites);
+/* output
+[
+  [ 'Seafood', 'Salad', 'Nugget', 'Soup' ],
+  [ 'Cake', 'Pie', 'Donut' ]
+]
+*/
+
+console.log(allFaforites1);
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup', 'Cake', 'Pie', 'Donut' ]
+*/
+
+const obj1 = {firstname: 'Obi-Wan', age: 30};
+const obj2 = {lastname: 'Kenobi', gender: 'M'};
+
+newObj = {...obj1, ...obj2};
+
+console.log(newObj);
+/* output
+{ firstName: 'Obi-Wan', age: 30, lastName: 'Kenobi', gender: 'M' }
 */
