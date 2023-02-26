eacf493dd98c97fc8d103d99073122ea1077bfc5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-27 06:34:01 +0700

    fundamental JS | WeakMap & WeakSet

--- weakMapDanWeakSet.js
+++ weakMapDanWeakSet.js
@@ -3,20 +3,43 @@
 let visitCountMap = new Map(); // Menyimpan Daftar User
 
 function countUser(user) {
-    let count = visitCountMap.get(user) || 0;
-    visitCountMap.set(user, count + 1);
+  let count = visitCountMap.get(user) || 0;
+  visitCountMap.set(user, count + 1);
 }
 
-let jonas = {name: "Jonas"};
+let jonas = { name: "Jonas" };
 countUser(jonas); // Menambahkan user "Jonas"
 
 jonas = null; // Data object "Jonas" dihapus
 
 // delay dibutuhkan untuk menunggu garbage collector bekerja
-setTimeout(function() {
-    console.log(visitCountMap);
-}, 10000)
+setTimeout(function () {
+  console.log(visitCountMap);
+}, 10000);
 
 /* output
 Map(1) { { name: 'Jonas' } => 1 }
+*/
+
+const { inspect } = require("util");
+
+let visitCountMap1 = new WeakMap();
+
+function countUser(user) {
+  let count = visitCountMap.get(user) || 0;
+  visitsCountMap.set(user, count + 1);
+}
+
+let jonas1 = { name: "Jonas" };
+countUser(jonas); // Menambahkan user "Jonas"
+
+jonas = null; // Data object "Jonas" dihapus
+
+// delay dibutuhkan untuk menunggu garbage collector bekerja
+setTimeout(function () {
+  console.log(inspect(visitCountMap, { showHidden: true }));
+}, 10000);
+
+/* output
+  WeakMap {  }
 */
