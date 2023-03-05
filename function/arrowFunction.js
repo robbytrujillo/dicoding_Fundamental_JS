5825f2649b28b1cb0f15bba0d099c2617bbef6e7
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-05 08:47:39 +0700

    fundamental JS | Function - arrowfunction

---
+++ function/arrowFunction.js
@@ -0,0 +1,22 @@
+// Arrow Function
+
+//1.   Regular Function
+// function declaration
+function sayHello(greet) {
+    console.log(`${greet}!`);
+}
+
+// function expression
+const sayName = function (name) {
+    console.log(`Nama saya ${name}`)
+}
+
+//2. Arrow Function
+// function expression
+const sayHello = (greet) => {
+    console.log(`${greet}!`)
+}
+
+const sayName1 = (name1) => {
+    console.log(`Nama saya ${name1}`)
+}
