8aaaba44a80af5afd24ec9425b87755da0a78f79
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-06 04:46:43 +0700

    fundamental JS | Function - closure

--- function/closure.js
+++ function/closure.js
@@ -15,3 +15,20 @@ init();
 /* output
 Halo, 'Robby Ilhkus'
  */
+
+function init1() {
+    const name1 = 'Robby Ilhkus'; // variable lokal di dalam scope fungsi init
+
+
+function greet1() {  // Inner function, merupakan contoh closure
+    console.log(`Halo, ${name1}`);  // Inner function, merupakan contoh closure
+}
+    return greet1;
+}
+
+const myFunction = init();
+myFunction();
+
+/* output
+Halo, 'Robby Ilhkus'
+ */
