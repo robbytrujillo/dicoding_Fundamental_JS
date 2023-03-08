d747385f975e0dabab20948a2ae9b7d915d7698d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-08 20:47:01 +0700

    fundamental JS | oop - oppjs

---
+++ function/knowledgeCheck.js
@@ -0,0 +1,8 @@
+function multiply(num) {
+    total = num * num;
+}
+
+const result = multiply(3);
+
+console.log(result);
+
---
+++ oop/oop.js
@@ -0,0 +1,19 @@
+// Object-Oriented Programming
+// ===========================
+
+
+const car = {
+    // properties
+    brand: 'Ford', 
+    color: 'red',
+    maxspeed: 200, 
+    chassisNumber: 'f-1',
+    // methods
+    drive: () =>{
+        console.log('driving');
+    },
+    reverse: () => {
+        console.log('reversing');
+    },
+    
+}
