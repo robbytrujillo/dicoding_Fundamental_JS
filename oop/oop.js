ccfff4aa3c57eaae1088719506e218b5ec3a664a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-09 06:11:28 +0700

    fundamental JS | oop - opp

--- oop/oop.js
+++ oop/oop.js
@@ -1,25 +1,63 @@
 // Object-Oriented Programming
 // ===========================
 
-
+// Object Car
 const car = {
-    // properties
-    brand: 'Ford', 
-    color: 'red',
-    maxspeed: 200, 
-    chassisNumber: 'f-1',
-    // methods
-    drive: () =>{
-        console.log('driving');
-    },
-    reverse: () => {
-        console.log('reversing');
-    },
-    turn: () => {
-        console.log('turning');
-    }
-
+  // properties
+  brand: "Ford",
+  color: "red",
+  maxspeed: 200,
+  chassisNumber: "f-1",
+  // methods
+  drive: () => {
+    console.log("driving");
+  },
+  reverse: () => {
+    console.log("reversing");
+  },
+  turn: () => {
+    console.log("turning");
+  }
 }
+
+
+const myCar = {
+  // properties
+  brand: "Tesla",
+  color: "black",
+  maxspeed: 250,
+  chassisNumber: "t-1",
+  // methods
+  drive: () => {
+    console.log("driving");
+  },
+  reverse: () => {
+    console.log("reversing");
+  },
+  turn: () => {
+    console.log("turning");
+  }
+}
+
+const yourCar = {
+  // properties
+  brand: "BMW",
+  color: "white",
+  maxspeed: 300,
+  chassisNumber: "b-1",
+  // methods
+  drive: () => {
+    console.log("driving");
+  },
+  reverse: () => {
+    console.log("reversing");
+  },
+  turn: () => {
+    console.log("turning");
+  }
+}
+
+
 
 console.log(car.brand); // Ford
 console.log(car.color); // red
