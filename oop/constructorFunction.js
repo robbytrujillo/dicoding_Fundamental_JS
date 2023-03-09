d6ef49d09171ba8342ea7ddc8ac89cdefa7999f5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-09 14:18:11 +0700

    fundamental JS | oop - Constructor Function

--- oop/constructorFunction.js
+++ oop/constructorFunction.js
@@ -1,28 +1,28 @@
 // Constructor Function
 
 function Car(brand, color, maxSpeed, chassisNumber) {
-    this.brand = brand;
-    this.color = color;
-    this.maxSpeed = maxSpeed;
-    this.chassisNumber = chassisNumber;
+  this.brand = brand;
+  this.color = color;
+  this.maxSpeed = maxSpeed;
+  this.chassisNumber = chassisNumber;
 }
 
-Car.prototype.drive = function() {
-    console.log(`${this.brand} ${this.color} is driving`);
-}
+Car.prototype.drive = function () {
+  console.log(`${this.brand} ${this.color} is driving`);
+};
 
-Car.prototype.reverse = function() {
-    console.log(`${this.brand} ${this.color} is reversing`);
-}
+Car.prototype.reverse = function () {
+  console.log(`${this.brand} ${this.color} is reversing`);
+};
 
-Car.prototype.turn = function() {
-    console.log(`${this.brand} ${this.color} is turning`);
-}
+Car.prototype.turn = function () {
+  console.log(`${this.brand} ${this.color} is turning`);
+};
 
 // Membuat objek mobil dengan constructor function Car
-const car1 = new Car('Toyota', 'Silver', 200, 'to-1');
-const car2 = new Car('Honda', 'Black', 180, 'ho-1');
-const car3 = new Car('Suzuki', 'Red', 220, 'su-1');
+const car1 = new Car("Toyota", "Silver", 200, "to-1");
+const car2 = new Car("Honda", "Black", 180, "ho-1");
+const car3 = new Car("Suzuki", "Red", 220, "su-1");
 
 console.log(car1);
 console.log(car2);
@@ -31,6 +31,59 @@ console.log(car3);
 car1.drive();
 car2.drive();
 car3.drive();
+
+/* Output
+Car { brand: 'Toyota', color: 'Silver', maxSpeed: 200, chassisNumber: 'to-1' }
+Car { brand: 'Honda', color: 'Black', maxSpeed: 180, chassisNumber: 'ho-1' }
+Car { brand: 'Suzuki', color: 'Red', maxSpeed: 220, chassisNumber: 'su-1' }
+ 
+Toyota Silver is driving
+Honda Black is driving
+Suzuki Red is driving
+*/
+
+const Car = (brand) => (this.brand = brand);
+
+const car4 = new Car("Toyota");
+
+/* Output
+TypeError: Car is not a constructor
+*/
+
+// Sintaks Class di ES6
+class Car {
+  constructor(brand, color, maxSpeed, chassisNumber) {
+    this.brand = brand;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+    this.chassisNumber = chassisNumber;
+  }
+
+  drive() {
+    console.log(`${this.brand} ${this.color} is driving`);
+  }
+
+  reverse() {
+    console.log(`${this.brand} ${this.color} is reversing`);
+  }
+
+  turn() {
+    console.log(`${this.brand} ${this.color} is turning`);
+  }
+}
+
+// Membuat objek mobil dengan constructor function Car
+const car5 = new Car("Toyota", "Silver", 200, "to-1");
+const car6 = new Car("Honda", "Black", 180, "ho-1");
+const car7 = new Car("Suzuki", "Red", 220, "su-1");
+
+console.log(car5);
+console.log(car6);
+console.log(car7);
+
+car5.drive();
+car6.drive();
+car7.drive();
 
 /* Output
 Car { brand: 'Toyota', color: 'Silver', maxSpeed: 200, chassisNumber: 'to-1' }
