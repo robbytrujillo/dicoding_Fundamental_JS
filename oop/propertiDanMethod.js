77ac327b435282c500587e81b875ae19aa179b11
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-10 09:59:34 +0700

    fundamental JS | oop - Properti dan Method

---
+++ oop/propertiDanMethod.js
@@ -0,0 +1,19 @@
+// Properti dan Method
+
+class Car {
+    constructor(brand, color, maxSpeed, chassisNumber) {
+        this.brand = brand;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.chassisNumber = chassisNumber;
+    }
+}
+
+class Mail {
+    constructor(sender, receiver, subject, body) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.subject = subject;
+        this.body = body;
+    }
+}
