3cc98f610b113a10a6878ef69045b0698d73a0f7
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-10 11:50:20 +0700

    fundamental JS | oop - Properti dan Method

--- oop/propertiDanMethod.js
+++ oop/propertiDanMethod.js
@@ -80,3 +80,36 @@ console.log(car7);
 Car { brand: 'BMW', color: 'red', maxSpeed: 200, chassisNumber: 'BMW-1' }
 */
 
+class User{
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+set fullName(fullName) {
+    const [firstName, lastName] = fullName.split('');
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+}
+
+const user - new User('John', 'Doe');
+console.log(user);
+console.log(user.fullName);
+
+user.fullName = 'Fulan Fulanah';
+console.log(user);
+console.log(user.fullName);
+
+/* Output:
+User { firstName: 'John', lastName: 'Doe' }
+John Doe
+User { firstName: 'Fulan', lastName: 'Fulanah' }
+Fulan Fulanah
+*/
+
+
