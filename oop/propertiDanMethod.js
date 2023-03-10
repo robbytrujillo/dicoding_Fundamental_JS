951da3157914ec49817df27b45a5537e13b9b16a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-10 10:09:10 +0700

    fundamental JS | oop - Properti dan Method

--- oop/propertiDanMethod.js
+++ oop/propertiDanMethod.js
@@ -17,3 +17,17 @@ class Mail {
         this.body = body;
     }
 }
+
+const car1 = new Car('BMW', 'red', 200, 'b-1');
+const car2 = new Car('audi', 'blue', 220, 'a-1');
+const car3 = new Car('BMW', 'red', 250, 'b-2');
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
+/* Output:
+Car { brand: 'BMW', color: 'red', maxSpeed: 200, chassisNumber: 'b-1' }
+Car { brand: 'Audi', color: 'blue', maxSpeed: 220, chassisNumber: 'a-1' }
+Car { brand: 'BMW', color: 'black', maxSpeed: 250, chassisNumber: 'b-2' }
+*/
