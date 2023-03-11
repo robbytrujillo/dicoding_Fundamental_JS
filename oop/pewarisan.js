e13000dcba2a2845c4b20854cc4fe6c256abba20
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-11 12:58:08 +0700

    fundamental JS | oop - pewarisan

--- oop/pewarisan.js
+++ oop/pewarisan.js
@@ -1,21 +1,70 @@
 // Pewarisan
-
-// EmailService
-class EmailService {
-    constructor(sender) {
-        this.sender = sender;
-    }
-
-    sendMessage(message, receiver) {
-        console.log(`${this.sender} sent ${message} to ${receiver}`);
-    }
-
-    sendDelayedMessage(message, receiver, delay) {
-        setTimeout(() => {
-            this.sendMessage(message, receiver);
-        }, delay);
-    }
-}
+// =========
 
 // WhatsAppService
+class WhatsAppService {
+  constructor(sender) {
+    this.sender = sender;
+  }
 
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+
+  sendBroadcastMessage(message, receivers) {
+    for (const receiver of receivers) {
+      this.sendMessage(message, receiver);
+    }
+  }
+}
+
+// EmailService
+class EMailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+
+  sendDelayedMessage(message, receiver, delay) {
+    setTimeout(() => {
+      this.sendMessage(message, receiver);
+    }, delay);
+  }
+
+  sendBroadcastMessage(message, receivers) {
+    for (const receiver of receivers) {
+      this.sendMessage(message, receiver);
+    }
+  }
+}
+
+class MailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+
+  sendDelayedMessage(message, receiver, delay) {
+    setTimeout(() => {
+      this.sendMessage(message, receiver);
+    }, delay);
+  }
+
+  sendBroadcastMessage(message, receivers) {
+    for (const receiver of receivers) {
+      this.sendMessage(message, receiver);
+    }
+  }
+}
+
+const whatsapp = new MailService("+6281234567890");
+const email = new MailService("dimas@dicoding.com");
+
+whatsapp.sendDelayedMessage();
+email.sendBroadcastMessage();
