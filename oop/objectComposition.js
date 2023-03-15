10239a73b6a9d669accb6bbaa72b20e237fa6480
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-16 06:54:43 +0700

    fundamental JS | Object Composition

--- oop/objectComposition.js
+++ oop/objectComposition.js
@@ -2,46 +2,78 @@
 // ==================
 
 class Developer {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    commitChanges() {
-        console.log(`{this.name} is comitting changes...`);
-    }
+  commitChanges() {
+    console.log(`{this.name} is comitting changes...`);
+  }
 }
 
 class FrontEndDeveloper extends Developer {
-    buildUI() {
-        console.log(`{this.name} is building UI...`);
-    }
+  buildUI() {
+    console.log(`{this.name} is building UI...`);
+  }
 }
 
 class BackEndDeveloper extends Developer {
-    buildAPI() {
-        console.log(`{this.name} is building API...`);
-    }
+  buildAPI() {
+    console.log(`{this.name} is building API...`);
+  }
 }
 
 class FrontEndDeveloper extends Developer {
-    deployApp() {
-        console.log(`{this.name} is deploying App...`);
-    }
+  deployApp() {
+    console.log(`{this.name} is deploying App...`);
+  }
 }
 
 class FullStackDeveloper extends Developer {
-    buildUI() {
-        console.log(`{this.name} is building UI...`);
-    }
+  buildUI() {
+    console.log(`{this.name} is building UI...`);
+  }
 
-    buildAPI() {
-        console.log(`{this.name} is building API...`);
-    }
+  buildAPI() {
+    console.log(`{this.name} is building API...`);
+  }
 
-    deployApp() {
-        console.log(`{this.name} is deploying App...`);
+  deployApp() {
+    console.log(`{this.name} is deploying App...`);
+  }
+}
+
+// Solusinya meninggalkan pewarisan dan gunakan teknik object composition
+class Developer1 {
+  constructor(name) {
+    this.name = name;
+  }
+
+  commitChanges() {
+    console.log(`${this.name} is commiting changes...`);
+  }
+}
+
+function canBuildUI(developer) {
+    return {
+        buildUI: () => {
+            console.log(`${developer.name} is building UI...`);
+        }
     }
 }
 
+function canBuildAPI(developer) {
+    return {
+        buildAPI: () => {
+            console.log(`${developer.name} is building API...`);
+        }
+    }
+}
 
-
+function canSeployApp(developer) {
+    return {
+        deployApp: () => {
+            console.log(`${developer.name} is deploying App...`);
+        }
+    }
+}
