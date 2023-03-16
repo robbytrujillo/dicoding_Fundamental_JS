800e873b93ecbe89508d9fc1af83d7e0874a2e67
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-16 07:00:37 +0700

    fundamental JS | Object Composition

--- oop/objectComposition.js
+++ oop/objectComposition.js
@@ -70,10 +70,31 @@ function canBuildAPI(developer) {
     }
 }
 
-function canSeployApp(developer) {
+function canDeployApp(developer) {
     return {
         deployApp: () => {
             console.log(`${developer.name} is deploying App...`);
         }
     }
+}
+
+// Fungsi Object menggunakan method Object.assign() 
+function createFrontEndDeveloper(name) {
+    const developer = new Developer(name);
+    return Object.assign(developer, canBuildUI(developer));
+}
+
+function createBackEndDeveloper(name) {
+    const developer = new Developer(name);
+    return Object.assign(developer, canBuildAPI(developer));
+}
+
+function createDevOps(name) {
+    const developer = new Developer(name);
+    return Object.assign(developer, canDeployApp(developer));
+}
+
+function createFullStackDeveloper(name) {
+    const developer = new Developer(name);
+    return Object.assign(developer, canBuildUI(developer), canBuildAPI(developer), canDeployApp(developer));
 }
