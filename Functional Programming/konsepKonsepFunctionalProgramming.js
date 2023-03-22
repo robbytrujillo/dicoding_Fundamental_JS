de3efa120a30f8299dbff25ae7125863b6f715a6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-22 14:53:50 +0700

    fundamental JS | Functional Programming - Konsep-konsep Functional Programming

--- Functional Programming/konsepKonsepFunctionalProgramming.js
+++ Functional Programming/konsepKonsepFunctionalProgramming.js
@@ -175,7 +175,30 @@ sayhello()();
  * Hello!
  */
 
+const names2 = ['Harry', 'Ron', 'Jeff', 'Thomas'];
 
+const arrayMap = (arr, action) => {
+    const loopTrough = (arr, action, newArray = [], index = 0) => {
+        const item = arr[index];
+        if(!item) return newArray;
+        return loopTrough(arr, action, [...newArray, action(arr[index])], index + 1);
+    }
+    return loopTrough(arr, action);
+}
 
+const newNames = arrayMap(names, (name2) => `${name2}!`);
+
+console.log({
+    names,
+    newNames,
+});
+
+/**
+ * output:
+ * {
+ *   names: [ 'Harry', 'Ron', 'Jeff', 'Thomas' ],
+ *   newNames: [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]
+ * }
+ */
 
 
