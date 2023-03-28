caa090ea9b98d729bb761c6cda43d9f7c5f90a76
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-29 06:03:46 +0700

    fundamental JS | Module - Export & Import

---
+++ module/exportDanImport.js
@@ -0,0 +1,29 @@
+// Export & Import
+
+// state.js -> kode export module.export
+const coffeeStock = {
+    arabic: 100,
+    robusta: 150,
+    liberica: 200
+}
+
+module.exports = coffeeStock;
+
+// perintah node.state.js
+Module {
+    id: '.',
+    path: '/home/dicoding/Playground/javascript/CoffeeMachine',
+    exports: { arabica: 100. robusta: 150, liberica: 200},
+    parent: null,
+    filename: '/home/dicoding/Playground/javascript/CoffeeMachine/state.js',
+    loaded: false,
+    children: [],
+    paths: [
+        '/home/dicoding/Playground/javascript/CoffeeMachine/node_modules',
+    '/home/dicoding/Playground/javascript/node_modules',
+    '/home/dicoding/Playground/node_modules',
+    '/home/dicoding/node_modules',
+    '/home/node_modules',
+    '/node_modules'
+    ]
+}
