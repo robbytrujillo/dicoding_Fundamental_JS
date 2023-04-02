2e61011b0d70577c9b40904cd086c9ec05856316
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-02 23:38:30 +0700

    fundamental JS | Module - Export Beberapa Nilai Pada NodeJS

--- module/exportBeberapaNilaiPadaNode.js
+++ module/exportBeberapaNilaiPadaNode.js
@@ -27,5 +27,25 @@ Module {
     },
     parent: null,
     filename: '/home/dicoding/Playground/javascript/CoffeMachine/state.js',
+    loaded: false,
+    children: [],
+    paths: [
+        '/home/dicoding/Playground/javascript/CoffeMachine/node_modules',
+        '/home/dicoding/Playground/javascript/node_modules',
+        '/home/dicoding/Playground/node_modules',
+        '/home/dicoding/node_modules',
+        '/home/node_modules',
+        '/node_modules'
+    ]
 
 }
+
+const {coffeeStock1, isCoffeMachineReady1} = require('./state');
+
+console.log(coffeeStock1);
+console.log(isCoffeMachineReady1);
+
+/* output
+{ arabica: 100, robusta: 150, liberica: 200 }
+true
+*/
