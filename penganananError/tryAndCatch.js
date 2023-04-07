bbaf660a931ff8b20f2bf06194f09b6f9d68a0be
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-07 09:54:12 +0700

    fundamental JS | penanganan error - try and catch

--- penganananError/tryAndCatch.js
+++ penganananError/tryAndCatch.js
@@ -2,6 +2,16 @@
 
 try {
     // kode
+    console.log("Awal blok try");
+    errorCode;
+    console.log("Akhir blok try");
 } catch (error) {
     // error handling
+    console.log("Terjadi error!");
+    // console.log("Tidak terjadi eror, maka kode ini diabaikan");
 }
+
+/* output
+Awal blok try
+Akhir blok try
+*/
