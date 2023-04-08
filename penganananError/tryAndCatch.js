e787f904e1ba895ec3724d2cfe67cd0629658644
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-08 08:27:22 +0700

    fundamental JS | penanganan error - try and catch

--- penganananError/tryAndCatch.js
+++ penganananError/tryAndCatch.js
@@ -8,10 +8,20 @@ try {
 } catch (error) {
     // error handling
     console.log("Terjadi error!");
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
     // console.log("Tidak terjadi eror, maka kode ini diabaikan");
 }
 
 /* output
 Awal blok try
 Akhir blok try
+ReferenceError
+errorCode is not defined
+ReferenceError: errorCode is not defined
+    at file:///home/dicoding/Playground/javascript/CoffeeMachine/error.js:3:5
+    at ModuleJob.run (internal/modules/esm/module_job.js:152:23)
+    at async Loader.import (internal/modules/esm/loader.js:166:24)
+    at async Object.loadESM (internal/process/esm_loader.js:68:5)
 */
