9cdebca9b4f286e480c33b3afd797b2533e606b5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-21 08:20:22 +0700

    Asynchronous Process | setTimeout Function

---
+++ asynchronousProcess/setTimeoutFunction.js
@@ -0,0 +1,9 @@
+// setTimeout Function
+
+console.log('Selamat datang!');
+
+setTimeout(() => {
+    console.log('Terima kasih sudah mampir, silahkan datang kembali!');
+}, 3000)
+
+console.log('Ada yang bisa dibantu?');
