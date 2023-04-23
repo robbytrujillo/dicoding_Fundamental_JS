f06a8b68dbc6fd1e4f06ac36d37714f3ede530fb
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-23 15:13:03 +0700

    Asynchronous Process | Asynchronous Handling dengan Callback - Ubah fungsi getUsers

--- asynchronousProcess/asynchronousHandlingDenganCallback.js
+++ asynchronousProcess/asynchronousHandlingDenganCallback.js
@@ -18,3 +18,18 @@ function userCallback(users) {
 }
 
 getUsers(usersCallback);
+
+function getUsers1(isOffline1, callback1) {
+    // simulate network delay
+    setTimeout(() => {
+        const users1 = ['John', 'Jack', 'abigail'];
+
+        if (isOffline) {
+            callback1(new Error('cannot retrieve users due offline'), null);
+            return;
+        }
+
+        callback1(null, users);
+    }, 3000);
+
+    }
