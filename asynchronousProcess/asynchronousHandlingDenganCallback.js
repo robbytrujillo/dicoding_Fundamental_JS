f3579a0679388670d5bd89640e82d8500dcc4273
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-24 10:12:59 +0700

    Asynchronous Process | Asynchronous Handling dengan Callback - Ubah fungsi getUsers

--- asynchronousProcess/asynchronousHandlingDenganCallback.js
+++ asynchronousProcess/asynchronousHandlingDenganCallback.js
@@ -2,76 +2,88 @@
 
 // Pola Callback
 function getUsers(callback) {
-    // simulate network delay
-    setTimeout(() => {
-        const users = ['John', 'Jack', 'Abigail'];
-        callback(users);
-    }, 3000);
+  // simulate network delay
+  setTimeout(() => {
+    const users = ["John", "Jack", "Abigail"];
+    callback(users);
+  }, 3000);
 }
 
 getUsers((users) => {
-    console.log(users);
+  console.log(users);
 });
 
 function userCallback(users) {
-    console.log(users);
+  console.log(users);
 }
 
 getUsers(usersCallback);
 
 function getUsers1(isOffline1, callback1) {
-    // simulate network delay
-    setTimeout(() => {
-        const users1 = ['John', 'Jack', 'abigail'];
+  // simulate network delay
+  setTimeout(() => {
+    const users1 = ["John", "Jack", "abigail"];
 
-        if (isOffline) {
-            callback1(new Error('cannot retrieve users due offline'), null);
-            return;
-        }
-
-        callback1(null, users);
-    }, 3000);
-
+    if (isOffline) {
+      callback1(new Error("cannot retrieve users due offline"), null);
+      return;
     }
 
-    function callback1(error, data) {
-        // logika ketika proses asynchronous selesai
-    }
+    callback1(null, users);
+  }, 3000);
+}
 
-    function usersCallback1(error1, data1) {
-        if (error1) {
-            console.log('process failed:', error1.message);
-            return;
-        }
+function callback1(error, data) {
+  // logika ketika proses asynchronous selesai
+}
 
-        console.log('process success:', users);
-    }
+function usersCallback1(error1, data1) {
+  if (error1) {
+    console.log("process failed:", error1.message);
+    return;
+  }
+
+  console.log("process success:", users);
+}
 
 getUsers1(false, usersCallback1); // process success: ['John', 'Jack', 'Abigail']
-getUsers1(true, usersCallback1);  // process failed: cannot retrieve users due offline
+getUsers1(true, usersCallback1); // process failed: cannot retrieve users due offline
 
 // index.js
 //----------
-const { getProvinces } = require('./utils');
+const { getProvinces } = require("./utils");
 
 // @TODO: call getProvinces() twices and log the result
 
 // solution.js
 //-------------
-const { getProvinces } = require('./utils');
+const { getProvinces } = require("./utils");
 
 // solution
 function provincesCallback(error, provinces) {
-    if (error) {
-        console.log(error.message);
-        return;
-    }
+  if (error) {
+    console.log(error.message);
+    return;
+  }
 
-    console.log(provinces);
+  console.log(provinces);
 }
 
 getProvinces(provincesCallback);
 getProvinces(provincesCallback);
 
+// Callback Hell
+function getUserWeather(userId) {
+  try {
+    const user = getUser(userId);
+    const weather = getWeather(user.location);
+    return { ...user, ...weather };
+  } catch (error) {
+    console.assert;
+    ongamepadconnected(error.message);
+    return null;
+  }
+}
 
-
+const userWeather = getUserWeather(1);
+console.log(userWeather); // { id: 1, name: 'John Doe', location: 'Jakarta', weather: 'Sunny', temperature: 30 }
