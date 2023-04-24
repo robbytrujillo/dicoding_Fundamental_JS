94ef49b3c95447e9474107ac89b9cfc648339a94
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-24 17:05:24 +0700

    Asynchronous Process | Asynchronous Handling dengan Callback - Ubah fungsi getUsers

--- asynchronousProcess/asynchronousHandlingDenganCallback.js
+++ asynchronousProcess/asynchronousHandlingDenganCallback.js
@@ -87,3 +87,34 @@ function getUserWeather(userId) {
 
 const userWeather = getUserWeather(1);
 console.log(userWeather); // { id: 1, name: 'John Doe', location: 'Jakarta', weather: 'Sunny', temperature: 30 }
+
+// index.js
+//----------
+
+const {
+    getUser, getWeather
+} = require('./utils')
+
+function getUserWeather(userId, callback) {
+    getUser(userId, (error, user) => {
+        if (error) {
+            callback(error, null);
+        }
+
+        getWeather(user.location, (error, weather) => {
+           if (error) {
+            callback(error, null);
+           } 
+
+           callback(null, {...user, ...weather });
+        });
+    });
+}
+
+getUserWeather(1, (error, userWeather) => {
+    if (error) {
+        console.log(error);
+    }
+
+    console.log(userWeather);  // { id: 1, name: 'John Doe', location: 'Jakarta', weather: 'Sunny', temperature: 30 }
+});
