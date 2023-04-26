f26abf1f575d53e7c923fddb0d5d5bd5be702107
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-26 18:34:53 +0700

    Asynchronous Process | Asynchronous Handling dengan Promise - fungsi getUsers

--- asynchronousProcess/asynchronousHandlingDenganPromise.js
+++ asynchronousProcess/asynchronousHandlingDenganPromise.js
@@ -15,3 +15,25 @@ function getUsers(isOffline, callback) {
         callback(null, users);
     }, 3000);
 }
+
+// ubah pendekatannya dengan menggunakan Promise fungsi getUser()
+function getUsers(isOffline) {
+
+    // return a Promise object
+    return new Promise((resolve, reject) => {
+        
+        // simulate network delay
+    setTimeout(() => {
+        const users = ['John', 'Jack', 'Abilagil'];
+
+        if (isOffline) {
+            callback(new Error('cannot retrive users due offline'));
+            return;
+        }
+
+        resolve(users);
+    }, 3000);
+
+
+    });
+}
