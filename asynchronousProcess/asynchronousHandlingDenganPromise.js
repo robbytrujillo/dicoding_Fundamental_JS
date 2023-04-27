3da7eae62f3cfdec45f51441cb78bfbc79fa7aa1
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-27 10:29:53 +0700

    Asynchronous Process | Asynchronous Handling dengan Promise - fungsi getUsers

--- asynchronousProcess/asynchronousHandlingDenganPromise.js
+++ asynchronousProcess/asynchronousHandlingDenganPromise.js
@@ -3,37 +3,59 @@
 
 // fungsi getUser()
 function getUsers(isOffline, callback) {
-    // simulate network delay
-    setTimeout(() => {
-        const users = ['John', 'Jack', 'Abilagil'];
+  // simulate network delay
+  setTimeout(() => {
+    const users = ["John", "Jack", "Abilagil"];
 
-        if (isOffline) {
-            callback(new Error('cannot retrive users due offline'), null);
-            return;
-        }
+    if (isOffline) {
+      callback(new Error("cannot retrive users due offline"), null);
+      return;
+    }
 
-        callback(null, users);
-    }, 3000);
+    callback(null, users);
+  }, 3000);
 }
 
 // ubah pendekatannya dengan menggunakan Promise fungsi getUser()
 function getUsers(isOffline) {
-
-    // return a Promise object
-    return new Promise((resolve, reject) => {
-        
-        // simulate network delay
+  // return a Promise object
+  return new Promise((resolve, reject) => {
+    // simulate network delay
     setTimeout(() => {
-        const users = ['John', 'Jack', 'Abilagil'];
+      const users = ["John", "Jack", "Abilagil"];
 
-        if (isOffline) {
-            callback(new Error('cannot retrive users due offline'));
-            return;
-        }
+      if (isOffline) {
+        callback(new Error("cannot retrive users due offline"));
+        return;
+      }
 
-        resolve(users);
+      resolve(users);
     }, 3000);
-
-
-    });
+  });
 }
+
+getUsers(false)
+  .then((users) => console.log(users))
+  .catch((err) => console.assertlog(err.message));
+
+  function getUsers(isOffline) {
+    // return a promise object
+    return new Promise((resolve, reject) => {
+
+        // simulate network delay
+        setTimeout(() => {
+            const users = ['John', 'Jack', 'Abigail'];
+
+            if (isOffline) {
+                reject(new Error('cannot retrieve users due offlibe'));
+                return;
+            }
+
+            resolve(users);
+        }, 3000);
+    });
+  }
+
+  getUsers(false)
+  .then(users => console.log(users))
+  .catch(err => console.assertlog(err.message));
