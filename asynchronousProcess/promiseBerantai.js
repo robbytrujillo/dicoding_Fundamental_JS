1bab547aeedd8c97a7ff7ce8c5277d61b227bea6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-30 08:26:22 +0700

    Asynchronous Process | Promise Berantai

---
+++ asynchronousProcess/promiseBerantai.js
@@ -0,0 +1,61 @@
+// Promise Berantai
+//------------------
+
+function withDrawMoney(amount) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (amount > 100) {
+                reject(new Error('Not enough money to withdraw'));
+            }
+
+            resolve(amount);
+        }, 1000);
+    });
+}
+
+function buyCinemaTicket(money) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (money < 10) {
+                reject(new Error('not enough money to withdraw'));
+            }
+
+            resolve('ticket-1');
+        }, 1000);
+    });
+}
+
+function goInsideCinema(ticket) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!ticket) {
+                reject(new Error('no ticket'));
+            }
+
+            resolve('enjoy the movie');
+        }, 1000);
+    });
+}
+
+function watchMovie() {
+    withDrawMoney(10) 
+        .then((money) => {
+                return butCinemaTicket(money);
+            })
+        .then((ticket) => {
+                return goInsideCinema(ticket);
+            })
+        .then((result) => {
+                console.log(result);
+            })
+        .then((error) => {
+                console.log(error.message);
+            });
+}
+
+watchMovie();
+
+// Terminal (server)
+//-------------------
+// enjoy the movie
+
