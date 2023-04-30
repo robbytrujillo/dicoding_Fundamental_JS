711bddd96252dbfdd51f082675bce9a5b0dd1418
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-04-30 09:08:00 +0700

    Asynchronous Process | Promise Berantai

--- asynchronousProcess/promiseBerantai.js
+++ asynchronousProcess/promiseBerantai.js
@@ -59,3 +59,11 @@ watchMovie();
 //-------------------
 // enjoy the movie
 
+function watchMovie() {
+    withDrawMoney(10)
+    .then((money) => buyCinemaTicket(money))
+    .then((ticket) => goInsideCinema(ticket))
+    .then((result) => console.log(result))
+    .then((error) => console.log(error.message));
+}
+
