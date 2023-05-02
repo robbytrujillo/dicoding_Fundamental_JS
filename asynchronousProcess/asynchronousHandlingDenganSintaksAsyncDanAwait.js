4542a70462d97aade0f4e606ce733263abf441df
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-02 10:48:29 +0700

    Asynchronous Process | Asynchronous Handling dengan Sintaks Async dan Await

--- asynchronousProcess/asynchronousHandlingDenganSintaksAsyncDanAwait.js
+++ asynchronousProcess/asynchronousHandlingDenganSintaksAsyncDanAwait.js
@@ -54,3 +54,43 @@ watchMovie(10)
 watchMovie(5)
   .then((result) => console.log(result)) // enjoy the movie
   .catch((error) => console.log(error.message)); // enjoy the movie
+
+
+// utils.js
+function withDrawMoney(amount) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            (amount > 100) {
+                reject(new Error('Not enough money to withdraw'))
+            }
+
+            resolve(amount)
+        }, 1000);
+    });
+}
+
+function buyCinemaTicket(money) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (money < 10) {
+                reject(new Error('not enough money to buy ticket'))
+            }
+
+            resolve('ticket-1')
+        }, 1000);
+    });
+}
+
+function goInsideCinema(ticket) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!ticket) {
+                reject(new Error('no ticket'))
+            }
+
+            resolve('enjoy the movie')
+        }, 1000);
+    });
+}
+
+module.exports = { withDrawMoney, buyCinemaTicket, goInsideCinema };
