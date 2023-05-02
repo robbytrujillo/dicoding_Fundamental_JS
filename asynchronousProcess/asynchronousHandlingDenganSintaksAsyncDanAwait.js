ea456cf3abd6b437080108e59cbac7588f968f36
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-02 10:24:19 +0700

    Asynchronous Process | Asynchronous Handling dengan Sintaks Async dan Await

--- asynchronousProcess/asynchronousHandlingDenganSintaksAsyncDanAwait.js
+++ asynchronousProcess/asynchronousHandlingDenganSintaksAsyncDanAwait.js
@@ -2,30 +2,55 @@
 //------------------------------------------------------
 
 function watchMovie() {
-    withDrawMoney(10)
-        .then((money) => buyCinemaTicket(money))
-        .then((ticket) => goInsideCinema(ticket))
-        .then((result) => console.log(result))
-        .then((error) => console.log(error.message));
+  withDrawMoney(10)
+    .then((money) => buyCinemaTicket(money))
+    .then((ticket) => goInsideCinema(ticket))
+    .then((result) => console.log(result))
+    .then((error) => console.log(error.message));
 }
 watchMovie();
 
 // jika kita memmanfaatkan async dan await dalam menangani proses asynchronous
 async function watchMovie() {
-    try {
-        const money =  await withDrawMoney(10);
-        const ticket =  await buyCinemaTicket(money);
-        const result =  await goInsideCinema(ticket);
+  try {
+    const money = await withDrawMoney(10);
+    const ticket = await buyCinemaTicket(money);
+    const result = await goInsideCinema(ticket);
 
-        console.log(result);
-    } catch (error) {
-        console.log(error.message);
-    }
-    }
-watchMovie().then(() => console.log('done'));
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+watchMovie().then(() => console.log("done"));
 
 /** output */
 // enjoy the movie
 // done
 
+// mengubah fungsi watchMovie() dgn mengembalikan nilai dan men-throw error
+//--------------------------------------------------------------------------
 
+// index.js
+//----------
+const { withDrawMoney, buyCinemaTicket, goInsideCinema } = require("./utils");
+
+async function watchMovie(amount) {
+  try {
+    const money = await withDrawMoney(amount);
+    const ticket = await buyCinemaTicket(money);
+    const result = await goInsideCinema(ticket);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+watchMovie(10)
+  .then((result) => console.log(result)) // enjoy the movie
+  .catch((error) => console.log(error.message)); // enjoy the movie
+
+watchMovie(5)
+  .then((result) => console.log(result)) // enjoy the movie
+  .catch((error) => console.log(error.message)); // enjoy the movie
