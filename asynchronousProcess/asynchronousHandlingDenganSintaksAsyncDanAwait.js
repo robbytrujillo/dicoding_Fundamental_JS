e6386cb202e7c883b2d5145fdce9126d1556450d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-02 09:57:00 +0700

    Asynchronous Process | Asynchronous Handling dengan Sintaks Async dan Await

--- asynchronousProcess/asynchronousHandlingDenganSintaksAsyncDanAwait.js
+++ asynchronousProcess/asynchronousHandlingDenganSintaksAsyncDanAwait.js
@@ -8,5 +8,21 @@ function watchMovie() {
         .then((result) => console.log(result))
         .then((error) => console.log(error.message));
 }
-
 watchMovie();
+
+// jika kita memmanfaatkan async dan await dalam menangani proses asynchronous
+async function watchMovie() {
+    try {
+        const money =  await withDrawMoney(10);
+        const ticket =  await buyCinemaTicket(money);
+        const result =  await goInsideCinema(ticket);
+
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+    }
+watchMovie();
+
+
+
