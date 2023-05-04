f9aeff809cb0514a2799eafe8c80d20a566ce4dd
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-04 10:44:37 +0700

    Asynchronous Process | Rangkuman Asynchronous Process

--- asynchronousProcess/rangkumanAsynchronousProcess.js
+++ asynchronousProcess/rangkumanAsynchronousProcess.js
@@ -41,3 +41,55 @@ getUserWeather(1, (error, getUserWeather) => {
 
     console.log(userWeather);
 });
+
+//3. memanfaatkan Promise dalam proses asynchronous
+const { getUsers, getWeather } = require('./utils');
+
+function getUserWeather(userId) {
+    let user;
+
+    return getUser(userId)
+        .then((_user) => {
+            user = _user;
+            return getWeather(user.location)
+        })
+        .then((weather) => ({ ...user, ...weather }))
+}
+
+getUserWeather(1)
+    .then(console.log)
+    .catch(console.log);
+
+//4. Asynchronous Handling dengan Promise
+/*
+Promise di JavaScript pun memiliki konsep yang sama karena memiliki beberapa kondisi, yaitu pending, fulfilled, dan rejected.
+
+Pending merupakan keadaan Promise sedang berjalan.
+Fulfilled merupakan keadaan Promise yang terpenuhi.
+Rejected merupakan keadaan Promise yang gagal terpenuhi.
+*/
+
+function getUsers(isOffline) {
+    // return a Promise object
+    return new Promise((resolve, reject) => {
+
+        // simulate network delay
+        setTimeout(() => {
+            const users = ['John', 'Jack', 'Abigail'];
+
+            if (isOffline) {
+                reject(new Error('cannot retrieve users due offline'));
+                return;
+            }
+
+            resolve(users);
+        }, 3000);
+    });
+}
+
+// mengembalikan object Promise
+getUsers(false)
+    .then(users => console.log(users))
+    .catch(err => console.log(err.message));
+
+//5. Mengubah Callback menjadi promise
